e96029f28dfc91584940937659c847b6bc49eceb
David Nunez <arizonatribe@gmail.com>
2018-11-01 12:35:13 -0700

    Changed dependency requiremnt onto a different package. Some code cleanup

--- index.js
+++ index.js
@@ -1,58 +1,51 @@
-const workerize = require('workerize')
+const makeWorker = require('web-worker')
 
-module.exports = workerize(`
-    export function fetchJson(config) {
-        const {
-            url,
-            token,
-            query,
-            params,
-            endpoint,
-            headers = {},
-            method = 'get',
-            ...restOfConfig
-        } = config || {}
-        if (!url) {
-            return Promise.reject('No URL was provided')
-        }
-        if (token) {
-            headers.authorization = 'Bearer ' + token
-        }
-        let fullUrl = url || restOfConfig.baseURL || restOfConfig.baseUrl
-        if (endpoint && typeof endpoint === 'string') {
-            fullUrl += endpoint.replace(/ /g, '-')
-        }
-        if (params) {
-            let queryString = '?'
-            for (let key in params) {
-                const val = params[key]
-                queryString += key + '=' + val
-            }
-            if (queryString !== '?') {
-                fullUrl += queryString
-            }
-        }
-        const options = {
-            ...restOfConfig,
-            method,
-            headers: {
-                ...headers,
-                'content-type': 'application/json'
-            }
-        }
-        delete options.headers['Content-Type']
-        if (/get/i.test(options.method)) {
-            options.body = config.body || config.data || {}
-        }
-        if (query) {
-            options.body = {
-                ...(options.body || {}),
-                query
-            }
-        }
-        if (options.body) {
-            options.body = JSON.stringify(options.body)
-        }
-        return fetch(fullUrl, options).then(response => response.json())
+module.exports = makeWorker(`
+  export function fetchJson(config = {}) {
+    const withoutTheseKeys = ['url', 'data', 'token', 'query', 'params', 'baseURL', 'baseUrl', 'endpoint']
+
+    if (!config.url) {
+      return Promise.reject('No URL was provided')
     }
+
+    let fullUrl = config.url || config.baseURL || config.baseUrl
+    if (config.endpoint && typeof config.endpoint === 'string') {
+      fullUrl += config.endpoint.replace(/ /g, '-')
+    }
+
+    if (config.params) {
+      let queryString = '?'
+      for (let key in config.params) {
+        const val = config.params[key]
+        queryString += key + '=' + val
+      }
+      if (queryString !== '?') {
+        fullUrl += queryString
+      }
+    }
+
+    const options = { headers: {} }
+    for (let key in config) {
+      if (withoutTheseKeys.indexOf(key) === -1) {
+        options[key] = config[key]
+      }
+    }
+    delete options.headers['Content-Type']
+    options.headers['content-type'] = 'application/json' 
+    if (config.token) {
+      options.headers.authorization = 'Bearer ' + config.token
+    }
+
+    if (/get/i.test(options.method)) {
+      options.body = config.body || config.data || {}
+    }
+    if (options.body) {
+      if (config.query) {
+        options.body.query = config.query
+      }
+      options.body = JSON.stringify(options.body)
+    }
+
+    return fetch(fullUrl, options).then(response => response.json())
+  }
 `, {type: 'module'})
